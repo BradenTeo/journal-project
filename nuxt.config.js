5049eb7b3672ed565241e5fb96443c9eae9ae6a8
BradenTeo <bteowr@hotmail.com>
2022-05-17 22:37:17 +0800

    Apply changes from Nuxt.js beginner tutorial

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    "@/assets/css/main.css"
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -40,7 +41,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    //'@nuxtjs/tailwindcss',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <Tutorial/>
+  <div>
+    <Title title="Home" />
+    <nuxt-link to="/about">About</nuxt-link>
+    <a href="http://youtube.com/tylerpotts">tylerpotts' youtube</a>
+
+    {{ $store.state.title }}
+
+    <button @click="increment">Increment Me! {{ $store.state.counter }}</button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'Home',
+  methods: {
+    increment() {
+      this.$store.commit('increment')
+    }
+  }
 }
 </script>
